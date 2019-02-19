4e909618205074f9dd197336e6a96a4076a1d62c
Samsoucy <newf930@outlook.com>
2019-02-19 16:54:45 -0500

    added get, returns a post object with specific id

--- posts/post-router.js
+++ posts/post-router.js
@@ -14,7 +14,24 @@ router.get('/', async (req, res) => {
         error: 'The posts information could not be retrieved.',
       });
     }
-  });
+});
+  
+router.get('/:id', async (req, res) => {
+  try {
+    const post = await Posts.findById(req.params.id);
+
+    if (post.length !== 0) {
+      res.status(200).json(post);
+    } else {
+      res.status(404).json({ message: "The post with the specified ID does not exist."  });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      error: "The post information could not be retrieved.",
+    });
+  }
+});
 
 
 
