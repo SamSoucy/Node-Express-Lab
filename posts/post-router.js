5533073084325869f991fed19d3e4c075bae8a56
Samsoucy <newf930@outlook.com>
2019-02-19 17:57:11 -0500

    added post, to create a post

--- posts/post-router.js
+++ posts/post-router.js
@@ -33,6 +33,23 @@ router.get('/:id', async (req, res) => {
   }
 });
 
+router.post('/', async (req, res) => {
+  try {
+    const post = await Posts.insert(req.body);
+    if (post) {
+      res.status(201).json(post);
+    } else {
+      res.status(400).json({ errorMessage: "Please provide title and contents for the post." });
+    }
+    
+  }catch (error) {
+    console.log(error);
+    res.status(500).json({
+      error: "There was an error while saving the post to the database",
+    });
+  }
+});
+
 
 
 
